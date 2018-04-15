e1d13578145c8eb4040b789dc78d2368b5b9cb05
Martin Kozma <martin@kozmatech.com>
2018-04-15 23:32:22 +1000

    Add project files.

---
+++ FerryO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FerryO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
